ac4c9dd95369a85bb6d05b3a482706efc91f5f37
svsriraj@gmail.com <svsriraj@gmail.com>
2026-09-06 06:56:18 +0000

    Fix RepositoriesMode reference in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(org.gradle.api.artifacts.repositories.RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
