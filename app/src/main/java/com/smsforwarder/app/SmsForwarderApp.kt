7b82b18a8492c8efd94b62f65988d8de31b72683
svsriraj@gmail.com <svsriraj@gmail.com>
2026-09-08 14:21:00 +0000

    Set Textbee API key in SmsForwarderApp.kt

--- app/src/main/java/com/smsforwarder/app/SmsForwarderApp.kt
+++ app/src/main/java/com/smsforwarder/app/SmsForwarderApp.kt
@@ -4,12 +4,52 @@ import android.app.Application
 import com.smsforwarder.app.utils.TextbeeService
 
 class SmsForwarderApp : Application() {
-    private var _apiKey: String? = null
+    private var _apiKey: String = "txb_GqpA3xNIDInaWQGye0DFlgPS1bVMW6sP"
 
     fun setApiKey(apiKey: String) {
         _apiKey = apiKey
         initializeApiService()
     }
+
+    fun getApiKey(): String = _apiKey
+
+    private fun initializeApiService() {
+        TextbeeService.initialize(this, _apiKey)
+    }
+
+    companion object {
+        lateinit var instance: SmsForwarderApp
+            private set
+
+        fun getInstance(): SmsForwarderApp = instance
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        initializeApiService()
+    }
+}
+
+    fun getApiKey(): String = _apiKey
+
+    private fun initializeApiService() {
+        TextbeeService.initialize(this, _apiKey)
+    }
+
+    companion object {
+        lateinit var instance: SmsForwarderApp
+            private set
+
+        fun getInstance(): SmsForwarderApp = instance
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        initializeApiService()
+    }
+}
 
     fun getApiKey(): String? = _apiKey
 
